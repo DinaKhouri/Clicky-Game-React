9a7f9eff0c5476dafd1a28531cea8d1a98d5e4c5
Dina Khouri <dina.khouri.91@gmail.com>
2019-04-30 12:57:13 -0500

    Updates

---
+++ precache-manifest.e2ef5c70217fabd7e00a5d0c09af83c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954bdf8605aa6b5b72181aa94e57fbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a739ea386a3d02da2f8",
+    "url": "/static/css/main.30607d77.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0a739ea386a3d02da2f8",
+    "url": "/static/js/main.ed0cac7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
