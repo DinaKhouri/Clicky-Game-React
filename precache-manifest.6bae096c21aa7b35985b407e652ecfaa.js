99a567a7d8bb5fc160a8a34dca806129e44bed1a
Dina Khouri <dina.khouri.91@gmail.com>
2019-04-30 11:33:05 -0500

    Updates

---
+++ precache-manifest.6bae096c21aa7b35985b407e652ecfaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e213a6b35253ee7c407fd0d90de790e",
+    "url": "/Clicky-Game-React/index.html"
+  },
+  {
+    "revision": "0a739ea386a3d02da2f8",
+    "url": "/Clicky-Game-React/static/css/main.30607d77.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game-React/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0a739ea386a3d02da2f8",
+    "url": "/Clicky-Game-React/static/js/main.ed0cac7e.chunk.js"
+  },
+  {
+    "revision": "7d0ac51346ea72d77976",
+    "url": "/Clicky-Game-React/static/js/runtime~main.889b2c19.js"
+  }
+]);
